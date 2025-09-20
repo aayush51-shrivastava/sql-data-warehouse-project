78ca6b6510a560c2d4ec7985559242b2ec62d109
aayush51-shrivastava <aayush51.shrivastava@gmail.com>
2025-09-20 11:40:57 +0530

    Update database name to data_warehouse_project in schema setup
    - Adjusted instructions and access/revoke statements
    - Dropped existing bronze, silver, gold schemas if present

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -3,13 +3,14 @@
   Warning: Drops existing bronze, silver, gold schemas
   Instructions: 
     1. Create the database if not already done:
-         create database data_warehouse;
-    2. Connect to 'data_warehouse' using a client like DataGrip before running
+         create database data_warehouse_project;
+    2. Connect to 'data_warehouse_project' using a client like DataGrip before
+  running
 */
 
 -- Restrict public access
 revoke create on schema public from public;
-revoke all on database data_warehouse from public;
+revoke all on database data_warehouse_project from public;
 
 -- Drop existing schemas
 drop schema if exists bronze cascade;
